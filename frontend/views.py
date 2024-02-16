8ff22358a0cc41443ae0002765ecb10573d12f0f
natalieconan <natalieconan17@gmail.com>
2024-02-16 13:20:04 +0700

    update csrf exempt for index.html

--- frontend/views.py
+++ frontend/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 
+@method_decorator(csrf_exempt, name="dispatch")
 class EcoinFrontendView(TemplateView):
     template_name = "index.html"
