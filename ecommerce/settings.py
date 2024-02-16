9cee405d80fb3609715d2c69d9cb62568738dafc
natalieconan <natalieconan17@gmail.com>
2024-02-16 13:08:41 +0700

    update csrf trusted origin

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -20,6 +20,7 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 ALLOW_ANONYMOUS = True
+CSRF_COOKIE_SECURE = True
 
 # DATE FORMAT
 DATE_INPUT_FORMATS = ['%d-%m-%Y']
