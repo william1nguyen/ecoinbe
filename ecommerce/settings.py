d2f9eed20bd07793a41a48801faa78cc5843d066
natalieconan <natalieconan17@gmail.com>
2024-02-16 11:09:01 +0700

    update env in settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -164,7 +164,7 @@ SIMPLE_JWT = {
     "BLACKLIST_AFTER_ROTATION": False,
     "UPDATE_LAST_LOGIN": False,
     "ALGORITHM": "HS256",
-    "SIGNING_KEY": SECRET_KEY,
+    "SIGNING_KEY": os.getenv("SECRET_KEY"),
     "VERIFYING_KEY": "",
     "AUDIENCE": None,
     "ISSUER": None,
