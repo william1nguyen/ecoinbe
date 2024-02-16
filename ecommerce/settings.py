9af126c6f9ec9e328f47637fd9c1cb2485f1d28e
natalieconan <natalieconan17@gmail.com>
2024-02-16 14:35:28 +0700

    remove Session Authentication

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -151,7 +151,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.BasicAuthentication",
-        "rest_framework.authentication.SessionAuthentication",
+        # "rest_framework.authentication.SessionAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
 }
