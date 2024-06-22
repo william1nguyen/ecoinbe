fc2df8bd6b4989e7cc7fb0b70f125eff0d0301fc
natalieconan <natalieconan17@gmail.com>
2024-06-22 10:32:59 +0700

    fix: update production settings

--- ecommerce/settings/production.py
+++ ecommerce/settings/production.py
@@ -2,23 +2,6 @@ from .base import *
 
 DEBUG = False
 
-# security.W016
-CSRF_COOKIE_SECURE = True
-
-# security.W012
-SESSION_COOKIE_SECURE = True
-
-# security.W008
-SECURE_SSL_REDIRECT = True
-
-# security.W004
-SECURE_HSTS_SECONDS = 31536000 # One year in seconds
-
-# Another security settings
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-SECURE_HSTS_PRELOAD = True
-SECURE_CONTENT_TYPE_NOSNIFF = True
-
 # Security
 ALLOWED_HOSTS = ["*"]
 CORS_ALLOW_ALL_ORIGINS = True
@@ -31,7 +14,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": os.getenv("DATABASE_NAME", "ecoin"),
         "USER": os.getenv("DATABASE_USER", "root"),
-        "PASSWORD": os.getenv("DATABASE_PASSWORD", ""),
+        "PASSWORD": os.getenv("DATABASE_PASSWORD", "password"),
         "HOST": os.getenv("DATABASE_HOST", "localhost"),
         "PORT": os.getenv("DATABASE_PORT", 3306),
         "OPTIONS": {
